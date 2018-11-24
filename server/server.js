aca530a434773a3ab3159cdcdf27ff4b47dcb9b2
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-11-24 13:47:07 +0100

    Add newMessage and createMessage event

--- server/server.js
+++ server/server.js
@@ -11,11 +11,36 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+function formatDate(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return `${hours}:${minutes}:${seconds} | ${day}-${month}-${year}r.`;
+}
+
 io.on("connection", socket => {
   console.log("New user connected");
 
   socket.on("disconnect", () => {
     console.log("User was disconnected");
+  });
+
+  socket.emit("newMessage", {
+    from: "Damian",
+    text: "O której się spotykamy?",
+    createdAt: formatDate(new Date())
+  });
+
+  socket.on("createMessage", message => {
+    console.log("createMessage", message);
+    socket.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: formatDate(new Date())
+    });
   });
 });
 
